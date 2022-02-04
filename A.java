5ef6603355fd513021e689e28d4b6419d81e1950
Valter Granato Neto <68205084+ValterGN@users.noreply.github.com>
2022-02-04 14:33:14 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,24 @@
+public class A {
+    private int A1;
+    private int A2;
+
+    public int getA1() {
+        return A1;
+    }
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+    public int getA2() {
+        return A2;
+    }
+    public void setA2(int a2) {
+        A2 = a2;
+    }
+    public void MA1(){
+        System.out.print("MA1");
+    }
+    public void MA2(){
+        System.out.print("MA2");
+    }
+    
+}
---
+++ B.java
@@ -0,0 +1,24 @@
+public class B {
+    private int B1;
+    private int B2;
+
+    public int getB1() {
+        return B1;
+    }
+    public void setB1(int b1) {
+        B1 = b1;
+    }
+    public int getB2() {
+        return B2;
+    }
+    public void setB2(int b2) {
+        B2 = b2;
+    }
+    public void MB1(){
+        System.out.print("MB1");
+    }
+    public void MB2(){
+        System.out.print("MB2");
+    }
+    
+}
